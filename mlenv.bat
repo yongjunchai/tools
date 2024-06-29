aa49b063db393a83fb66664e6f2c0e144d5cbd69
Yongjun Chai <yongjunchai@hotmail.com>
2024-06-29 15:15:40 -0700

    set conda environment variable

---
+++ mlenv.bat
@@ -0,0 +1,1 @@
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'e:\ProgramData\miniconda3\shell\condabin\conda-hook.ps1' ; conda activate 'e:\ProgramData\miniconda3' "
